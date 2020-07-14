7d88e1eb54e4bbdc0d991d7927e640b9f0b44213
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-07-14 14:14:56 +0100

    NO MAJOR CHANGES...SAVING

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=os.environ.get("IP"),
+    port=int(os.environ.get("PORT", 5000)),
+    debug=True)
