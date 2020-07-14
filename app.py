de91fb3d06ced0075badbdcd9a1de2f4097d2b61
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-07-14 15:51:55 +0100

    flask

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT", 5000)),
