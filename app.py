4b94e530156a8447cde41b0e7e9ad4a781f698de
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-07-20 16:03:30 +0100

    Flask App For Loop

--- app.py
+++ app.py
@@ -4,23 +4,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/about.html')
 def about():
-    return render_template('about.html', page_title="About")
+    return render_template('about.html', page_title="About", list_of_numbers=[1, 2, 3])
+
 
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html', page_title="Contact")
 
+
 @app.route('/careers.html')
 def careers():
     return render_template('careers.html', page_title="Careers")
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT", 5000)),
-    debug=True)
+            port=int(os.environ.get("PORT", 5000)),
+            debug=True)
