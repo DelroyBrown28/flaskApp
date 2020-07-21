03c3376f1e34d233e706e683638bbb09718fb9fe
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-07-21 14:45:58 +0100

    Flash App providing feedback to user

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
+app.secret_key = "some_secret"
 
 
 @app.route('/')
@@ -35,8 +36,9 @@ def about_member(member_name):
 @app.route('/contact.html', methods=['GET', 'POST'])
 def contact():
     if request.method == "POST":
-        print(request.form)
-        
+        flash("Thanks {}, we have received your message!".format(
+            request.form["email"]))
+
     return render_template('contact.html', page_title="Contact")
 
 
